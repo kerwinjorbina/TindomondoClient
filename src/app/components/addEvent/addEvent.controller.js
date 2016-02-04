585fddf810bd7722381f76172903f08ac8b06d4d
popovkos <popov4ik4@gmail.com>
2016-02-04 14:37:26 +0000

    scss changes panelmap

--- src/app/components/addEvent/addEvent.controller.js
+++ src/app/components/addEvent/addEvent.controller.js
@@ -8,11 +8,25 @@
   /** @ngInject */
   function addEventController($scope, $state) {
     var vm = this;
+    vm,
     vm.date;
     vm.sport;
     vm.participants;
     vm.place;
     vm.directionsService = new google.maps.DirectionsService();
+        vm.map = {
+      center: {
+        latitude : 58.3661916,
+        longitude : 26.69020660000001
+      },
+      bounds: new google.maps.LatLngBounds(),
+      zoom: 16,
+      options: {
+        mapTypeControl: true,
+        panControl: true,
+        zoomControl: true
+      }
+    };
 
   }
 })();
