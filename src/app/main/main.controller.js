06916ae196c8f565105689c9560e3b4dd9e8d1ed
kerwinjorbina <kerwinjorbina@kerwinjorbina-Inspiron-3442.(none)>
2016-02-04 12:40:31 +0200

    adding service and constant for backend link

--- src/app/main/main.controller.js
+++ src/app/main/main.controller.js
@@ -6,8 +6,17 @@
     .controller('MainController', MainController);
 
   /** @ngInject */
-  function MainController() {
+  function MainController(backendlink, eventService) {
     var vm = this;
-
+    eventService.getEvents(1).then(function(response) {
+		alert(response.data.length);
+		response.data.forEach(function(event) {
+          vm.orders.push(
+            {sport_id: event.sport_id, user_id: event.user_id, location: event.location}
+          );
+        console.log(response);
+        console.log(response);
+        console.log(response);
+  	});
   }
 })();
