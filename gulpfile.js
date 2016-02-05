7573a2a4efea72679a3c50c056ca88de39916206
Arne Lapõnin <arne.laponin@gmail.com>
2016-02-05 17:06:41 +0200

    Gulp config

--- gulpfile.js
+++ gulpfile.js
@@ -27,11 +27,3 @@ wrench.readdirSyncRecursive('./gulp').filter(function(file) {
 gulp.task('default', ['clean'], function () {
   gulp.start('build');
 });
-
-gulp.task('serveprod', function() {
-  connect.server({
-    root: '/',
-    port: process.env.PORT || 5000, // localhost:5000
-    livereload: false
-  });
-});
