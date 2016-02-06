2734c77b8b4a57b4e244dacb383dba9b0748d9ec
Arne Lapõnin <arne.laponin@gmail.com>
2016-02-06 12:29:09 +0200

    Removed ngTouch module

--- src/app/index.module.js
+++ src/app/index.module.js
@@ -4,7 +4,7 @@
 
 angular
     .module('client', ['ngMaterial', 'ngMessages', 'ngAnimate', 'ngCookies',
-      'ngRoute', 'ngTouch', 'ngSanitize', 'ngMessages', 'ngAria', 'ui.router',
+      'ngRoute', 'ngSanitize', 'ngMessages', 'ngAria', 'ui.router',
       'ui.bootstrap', 'uiGmapgoogle-maps', 'ui-notification', 'google.places', 'frapontillo.bootstrap-switch', 'angularMoment', 'facebook']);
 
 })();
