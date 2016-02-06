a17d9fc3181c53730630b4c76e0ca66c3416aa46
Karli Oruste <karli.oruste@gmail.com>
2016-02-06 13:48:34 +0000

    material design

--- src/app/index.constants.js
+++ src/app/index.constants.js
@@ -5,6 +5,11 @@
   angular
     .module('client')
     .constant('moment', moment)
+<<<<<<< Updated upstream
     .constant('backendlink', "http://tindomondo.com:3000/");
     //.constant('backendlink', "http://api.tindomondo.com/");
+=======
+    //.constant('backendlink', "http://localhost:8081/");
+    .constant('backendlink', "http://tindomondo.com:3000/");
+>>>>>>> Stashed changes
 })();
