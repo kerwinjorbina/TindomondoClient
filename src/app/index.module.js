b88e2c39524a046afb870f543c33ad8a8efec35c
Karli Oruste <karli.oruste@gmail.com>
2016-02-06 10:08:24 +0000

    angular createview

--- src/app/index.module.js
+++ src/app/index.module.js
@@ -3,6 +3,10 @@
 
 
 angular
+<<<<<<< Updated upstream
     .module('client', ['ngAnimate', 'ngCookies', 'ngRoute', 'ngTouch', 'ngSanitize', 'ngMessages', 'ngAria', 'ui.router', 'ui.bootstrap', 'uiGmapgoogle-maps', 'ui-notification', 'google.places', 'frapontillo.bootstrap-switch', 'angularMoment', 'facebook']);
+=======
+    .module('client', ['ngMaterial', 'ngMessages', 'ngAnimate', 'ngCookies', 'ngTouch', 'ngSanitize', 'ngMessages', 'ngAria', 'ui.router', 'ui.bootstrap', 'uiGmapgoogle-maps', 'ui-notification', 'google.places', 'frapontillo.bootstrap-switch', 'angularMoment', 'facebook']);
+>>>>>>> Stashed changes
 
 })();
